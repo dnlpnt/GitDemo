94db5c3796f2232c69fa02a466cfdcc6d05e15e5
Daniil Panteleev <danilapanteleev@MacBook-Pro-Daniil.local>
2022-06-07 14:29:45 +0300

    Initial commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Daniil Panteleev on 07.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
