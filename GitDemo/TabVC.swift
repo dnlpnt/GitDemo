85900ea9d9ef8a5bf00bebcc01cec0f6dc7110e0
danila <panted@mail.ru>
2022-06-22 15:40:43 +0300

    overview

---
+++ GitDemo/TabVC.swift
@@ -0,0 +1,21 @@
+//
+//  TabVC.swift
+//  GitDemo
+//
+//  Created by Daniil Panteleev on 22.06.2022.
+//
+
+import Foundation
+import UIKit
+
+
+class TabVC: UITabBarController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    
+  }
+  
+  
+}
